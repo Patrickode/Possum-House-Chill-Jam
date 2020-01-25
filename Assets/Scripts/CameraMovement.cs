343c7fde83c15e658b64d638324c4cb2b189050d
Patrick <prm5983@rit.edu>
2020-01-25 14:57:11 -0500

    Added start of camera movement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject focalObj;
+    public float camSpeed = 1;
+
+    void Update()
+    {
+        //Stick to focal point
+        transform.position = focalObj.transform.position;
+
+        //Smoothly swing to back of player
+        transform.rotation = Quaternion.Lerp(transform.rotation, focalObj.transform.rotation, Time.deltaTime);
+    }
+}
