11cbf959d2006b759b3dbc3d00ef99d023b59a16
Patrick <prm5983@rit.edu>
2020-01-26 19:27:51 -0500

    Added a death plane and a win cube, addec warp script that works for both

---
+++ Assets/Scripts/PlayerWarp.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerWarp : MonoBehaviour
+{
+	public enum WarpType
+	{
+		Death,
+		Win
+	}
+
+    public WarpType warpType;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            switch (warpType)
+            {
+                case WarpType.Death:
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+                    break;
+
+                case WarpType.Win:
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+                    break;
+
+                default:
+                    break;
+            }
+        }
+    }
+}
